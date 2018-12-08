ae741ca1c1b2f347f7c00dc33831d69cae3e3b17
“Waixi00008” <xiaobing00008@gmail.com>
2018-12-08 14:39:31 +0800

    vue-component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App'
 import router from './router'
+import TodoList from './components/TodoList'
 
 Vue.config.productionTip = false
 
@@ -16,46 +17,7 @@ Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  data: {
-    title: 'vue',
-    subtitle: '前端框架',
-    inputText: '',
-    todos: [{
-        text: '吃饭',
-        done: false
-      },
-      {
-        text: '睡觉',
-        done: false
-      },
-      {
-        text: '玩游戏',
-        done: false
-      },
-      {
-        text: '写代码',
-        done: false
-      },
-    ],
-  },
-  computed:{
-    remain() {
-      return this.todos.filter(v => !v.done).length
-    }
-  },
-  methods: {
-    handleClick() {
-      this.todos.push({
-        text: this.inputText,
-        done: false
-      })
-      this.inputText = ''
-    },
-    handleToggle(index) {
-      this.todos[index].done= !this.todos[index].done
-    },
-    handleClean() {
-      this.todos = this.todos.filter(v=>!v.done)
-    }
-  }
+  router,
+  components: { TodoList },
+  template: '<TodoList/>'
 })
