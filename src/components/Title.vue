a14372ab75afc01e58b750ed4a11f7ea12556849
“Waixi00008” <xiaobing00008@gmail.com>
2018-12-08 15:13:24 +0800

    vue-component

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="title">
+    <p>{{title}}</p>
+    <p>{{subtitle}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String
+  }
+}
+</script>
+
+<style scoped lang="css">
+.title {
+  color: red;
+}
+</style>
